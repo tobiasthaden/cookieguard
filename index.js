01501426eced64b8c464ee92d6b2aa159e6d9866
Tobias Thaden <hello@tobiasthaden.com>
2021-01-26 12:59:21 +0100

    Add package entry

---
+++ index.js
@@ -0,0 +1,8 @@
+import Cookieguard from "./src/Cookieguard";
+export default Cookieguard;
+
+import AbstractService from "./src/services/Service";
+export const Service = AbstractService;
+
+import GoogleAnalyticsService from "./src/services/GoogleAnalytics";
+export const GoogleAnalytics = GoogleAnalyticsService;
