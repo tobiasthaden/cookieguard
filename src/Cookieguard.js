87bce4503b4f1573303963c77d92f2e1e709ae1b
Tobias Thaden <hello@tobiasthaden.com>
2021-02-14 22:36:22 +0100

    Allow to update cookieguard from iframe

--- src/Cookieguard.js
+++ src/Cookieguard.js
@@ -6,7 +6,12 @@ export default class Cookieguard {
 
         this.fetchSettings();
 
+        this._listenForPostMessages();
+
         this.hydrate();
+
+        console.log(this.modules);
+        console.log(this.settings);
     }
 
     isExpired() {
@@ -47,6 +52,8 @@ export default class Cookieguard {
     handle(keys) {
         let modules = Object.keys(this.modules);
 
+        this._setValidPostMessageOrigins(modules);
+
         keys = keys || [];
 
         modules
@@ -56,5 +63,35 @@ export default class Cookieguard {
         modules
             .filter((item) => !keys.includes(item))
             .map((key) => tap(this.modules[key], (module) => module.disable()));
+    }
+
+    static updateFromIFrame(key) {
+        window.parent.postMessage({ cookieguard: {module: key}}, window);
+    }
+
+    _setValidPostMessageOrigins(modules) {
+        let origins = modules.map((key) => {
+            if (this.modules[key]['fallbackUrl']) {
+                let url = new URL(this.modules[key]['fallbackUrl']);
+                let { origin } = url;
+                return origin;
+            }
+        }).filter(origin => origin);
+
+        this.postMessageOrigins = origins;
+    }
+
+    _listenForPostMessages() {
+        window.addEventListener("message", (event) => {
+            if (!this.postMessageOrigins.includes(event.origin)) {
+                return;
+            }
+
+            if (event.data.cookieguard) {
+                this.settings.modules.push(event.data.cookieguard.module)
+                this.hydrate();
+            }
+
+          }, false);
     }
 }
