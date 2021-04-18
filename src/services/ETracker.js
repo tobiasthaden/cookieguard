e13acefde89186dbdd27033d0d38ef039c947796
Tobias Thaden <hello@tobiasthaden.com>
2021-04-18 12:18:00 +0200

    Add eTracker service

---
+++ src/services/ETracker.js
@@ -0,0 +1,31 @@
+import Service from './Service.js';
+
+export default class ETracker extends Service {
+    constructor(id, domain, path) {
+        super(domain, path);
+
+        this.id = id;
+
+        this._embedScript();
+    }
+
+    enable() {
+        _etracker.enableCookies(this.domain);
+    }
+
+    disable() {
+        _etracker.disableCookies(this.domain);
+    }
+
+    _embedScript() {
+        let script = document.createElement('script');
+        script.id = '_etLoader';
+        script.setAttribute('data-block-cookies', 'true');
+        script.setAttribute('data-respect-dnt', 'dnt');
+        script.setAttribute('data-secure-code', this.id);
+        script.src = 'https://static.etracker.com/code/e.js';
+        document
+            .getElementsByTagName('head')[0]
+            .appendChild(script, document.getElementsByTagName('head')[0]);
+    }
+}
